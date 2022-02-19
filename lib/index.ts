7fa65c35dca30b10f7863bcf9c726aa5da818293
Vinícius Gutierrez <srgutyerrez@gmail.com>
2022-02-18 23:41:04 -0300

    feat: add zeraph-database library

--- lib/index.ts
+++ lib/index.ts
@@ -7,9 +7,8 @@ export * from '@vyrnn/zeraph-http'
 export * from '@vyrnn/zeraph-provider'
 export * from '@vyrnn/zeraph-cache'
 export * from '@vyrnn/zeraph-util'
+export * from '@vyrnn/zeraph-database'
 
 export * from 'luxon'
 
-export {
-  Group
-}
+export { Group }
